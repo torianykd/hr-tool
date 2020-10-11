726f0424de8f767f3ccbefd0f297d9433344f65a
Dmitriy Toryanik <dmitriy.toryanik@codeit.com.ua>
2020-10-11 14:54:02 +0300

    Initial commit.

---
+++ src/main/java/ua/com/alevel/nix/hrtool/HrToolApplication.java
@@ -0,0 +1,13 @@
+package ua.com.alevel.nix.hrtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/com/alevel/nix/hrtool/HrToolApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.alevel.nix.hrtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
